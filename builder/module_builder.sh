7e02672ec50e284b875a2cab29c1e050f2073a00
vdbe <44153531+vdbe@users.noreply.github.com>
2024-08-04 14:16:57 +0000

    feat: releases (#260)
    * feat: bootstrap release please
    
    * docs: add instruction to install a tag/release via tpm
    
    * chore: add `tmux.conf` placeholder for bug template
    
    * docs: add CONTRIBUTING.md
    
    Copied from https://github.com/catppuccin/nix/blob/3fdc011242c684c48ed96d91e77c74e760aedc3e/CONTRIBUTING.md
    
    * refactor: change vlatest to latest
    
    * docs: remove merge instructions
    
    * docs: use full version as tag msg

--- builder/module_builder.sh
+++ builder/module_builder.sh
@@ -9,7 +9,7 @@ build_status_module() {
   if [ "$status_fill" = "icon" ]; then
     local bg
     local show_icon="#[fg=$thm_bg,bg=$color,nobold,nounderscore,noitalics]$icon "
-    local show_text="#[fg=$thm_fg,bg=$thm_gray] $text"
+    local show_text="#[fg=$thm_fg,bg=$thm_bg] $text"
 
     if [ "$status_connect_separator" = "yes" ]; then
       bg="$thm_gray"
@@ -18,7 +18,7 @@ build_status_module() {
     fi
 
     local show_left_separator="#[fg=$color,bg=$bg,nobold,nounderscore,noitalics]$status_left_separator"
-    local show_right_separator="#[fg=$thm_gray,bg=$bg,nobold,nounderscore,noitalics]$status_right_separator"
+    local show_right_separator="#[fg=$thm_bg,bg=$bg,nobold,nounderscore,noitalics]$status_right_separator"
   fi
 
   if [ "$status_fill" = "all" ]; then
